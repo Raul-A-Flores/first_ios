4190e74eec95ea95e5d0e440d0667b439af224a1
Raul Flores <raul.alexander.floress@gmail.com>
2023-07-01 20:44:05 -0500

    first apple app

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable{
+    case 🥳,🤩,😛,🤪
+}
+
 struct ContentView: View {
+    
+    @State var selection: Emoji = .🤪
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size:150))
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self){
+                        emoji in Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers:")
+            .padding()
+            
         }
-        .padding()
+      
     }
 }
 
